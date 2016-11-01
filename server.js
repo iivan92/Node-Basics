ae1175074a5b616a26fbf1756c303f04dfcc5519
Ivan Megias Duran <ivan92.md@gmail.com>
2016-11-02 00:25:37 +0100

    Added libraries for cookies and session in the server.

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var morgan = require('morgan');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var cookie = require('cookie-parser');
+var session = require('express-session');
+
 app.use(morgan('dev'));
+app.use(cookie());
+app.use(session({secret: 'basic server',
+				saveUninitialized: true,
+				resave: true}));
 
 app.use('/', function(req, res) {
 	res.send("Hello World!");
+	console.log(req.cookies);
+	console.log('=============');
+	console.log(req.session);
 });
 
 app.listen(port);
